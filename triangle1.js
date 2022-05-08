b6df6380589ce5dd5c27416292741f0d5daf2141
Vaishnavi <vaishnavivhora5@gmail.com>
2022-05-08 08:12:31 +0530

    updated

--- triangle1.js
+++ triangle1.js
@@ -1,9 +1,13 @@
-let n = 7;
-let string = "";
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += "#";
-  }
-  string += "\n";
-}
-console.log(string);
+// let n = 7;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     string += "#";
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+
+for (let i = 1; i <= 7; i++)
+ console.log("#".repeat(i));
+
